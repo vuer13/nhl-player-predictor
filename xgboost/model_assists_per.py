c4bddc82f6d4dd19e4857cc79cc08b6182e438b3
Ryan Vu <ryanvu1131@gmail.com>
2025-12-15 20:39:20 -0800

    typos and shap

--- xgboost/model_assists_per.py
+++ xgboost/model_assists_per.py
@@ -13,6 +13,7 @@ from sklearn.pipeline import make_pipeline
 
 import optuna
 import joblib
+import shap
 
 # Trimmed lags incase 4/5 don't do anything
 '''
@@ -25,6 +26,7 @@ import joblib
 
 def preprocessing(X):
     """Preprocessing pipeline for the model."""
+    
     numeric_features = X.columns.tolist()
 
     preprocessor = make_column_transformer(
@@ -47,16 +49,35 @@ def preprocessing(X):
     return pipe
 
 def feature_importance(pipe):
-    xgb_model = pipe.named_steps["xgbregressor"]
+    xgb = pipe.named_steps["xgbregressor"]
     feature_names = pipe.named_steps["columntransformer"].get_feature_names_out()
-    importances = xgb_model.feature_importances_
 
-    feat_imp = (
-        pd.Series(importances, index=feature_names)
-        .sort_values(ascending=False)
+    imp = pd.Series(
+        xgb.feature_importances_,
+        index=feature_names
+    ).sort_values(ascending=False)
+
+    print(imp.head(25))
+    return imp
+
+    
+def run_shap(pipe, X_valid):
+    X_val_trans = pipe.named_steps["columntransformer"].transform(X_valid)
+
+    explainer = shap.TreeExplainer(
+        pipe.named_steps["xgbregressor"]
     )
 
-    print(feat_imp)
+    shap_values = explainer.shap_values(X_val_trans)
+
+    feature_names = pipe.named_steps["columntransformer"].get_feature_names_out()
+
+    shap.summary_plot(
+        shap_values,
+        X_val_trans,
+        feature_names=feature_names
+    )
+
 
 def model():
     # Read csv file
@@ -90,35 +111,36 @@ def model():
         "corsi", "xGoalsForAfterShifts", "corsiForAfterShifts", "age2"
     ]
         
-    target = "log_apg"
-
     df_model = df
 
-    sample_weight = df_model["apg"].apply(lambda x: 6.25 if x > 0.9 else 1.0)
-
-    X = df_model[features]
-    Y = df_model[target]
-
-    X_train, X_valid, Y_train, Y_valid, w_train, w_valid = train_test_split(X, Y, sample_weight, test_size=0.2, random_state=42)
+    X = df[features]
+    y = df["log_apg"]
     
-    pipeline = preprocessing(X)
+    sample_weight = df["apg"].apply(lambda x: 6.25 if x > 0.9 else 1.0)
+    
+    X_train, X_valid, y_train, y_valid, w_train, w_valid = train_test_split(
+        X, y, sample_weight,
+        test_size=0.2,
+        random_state=42
+    )
+
+    pipe = build_pipeline(X)
 
     def estimate(trial):
         params = {
-            "n_estimators": trial.suggest_int("n_estimators", 300, 700),
-            "max_depth": trial.suggest_int("max_depth", 2, 3),
-            "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.05),
-            "subsample": trial.suggest_float("subsample", 0.6, 0.9),
-            "colsample_bytree": trial.suggest_float("colsample_bytree", 0.6, 0.9),
-            "reg_alpha": trial.suggest_float("reg_alpha", 1.5, 3.0),
-            "reg_lambda": trial.suggest_float("reg_lambda", 3.0, 9.0),
-            "min_child_weight": trial.suggest_int("min_child_weight", 8, 10),
-            "random_state": 0
-        }
+            "xgbregressor__n_estimators": trial.suggest_int("n_estimators", 300, 700),
+            "xgbregressor__max_depth": trial.suggest_int("max_depth", 2, 3),
+            "xgbregressor__learning_rate": trial.suggest_float("learning_rate", 0.01, 0.05),
+            "xgbregressor__subsample": trial.suggest_float("subsample", 0.6, 0.9),
+            "xgbregressor__colsample_bytree": trial.suggest_float("colsample_bytree", 0.6, 0.9),
+            "xgbregressor__reg_alpha": trial.suggest_float("reg_alpha", 1.5, 3.0),
+            "xgbregressor__reg_lambda": trial.suggest_float("reg_lambda", 3.0, 9.0),
+            "xgbregressor__min_child_weight": trial.suggest_int("min_child_weight", 8, 10),
+        }       
 
         pipeline.set_params(**params)
 
-        pipeline.fit(
+        pipe.fit(
             X_train,
             y_train,
             xgbregressor__sample_weight=w_train
@@ -130,11 +152,9 @@ def model():
     study = optuna.create_study(direction = "maximize")
     study.optimize(estimate, n_trials = 150)
 
-    pipeline.set_params(**study.best_params)
-    pipeline.fit(X_train, y_train, xgbregressor__sample_weight=w_train)
+    pipe.set_params(**study.best_params)
+    pipe.fit(X_train, y_train, xgbregressor__sample_weight=w_train)
     
-    feature_importance(pipeline)
-
     log_preds = pipe.predict(X_valid)
 
     y_pred = np.expm1(log_preds)
@@ -146,8 +166,11 @@ def model():
 
     print("Train R² (log):", r2_score(y_train, pipe.predict(X_train)))
     print("Val R² (log):", r2_score(y_valid, pipe.predict(X_valid)))
+    
+    feature_importance(pipe)
+    run_shap(pipe, X_valid)
 
-    joblib.dump(xgb, "../models/xgb_model_assists.pkl")
+    joblib.dump(pipe, "../models/xgb_model_assists.pkl")
     print("Model Saved")
 
 if __name__ == '__main__':
